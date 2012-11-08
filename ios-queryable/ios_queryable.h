e2324356f44005e81098c083a5bcd5d0e5beddbd
Marty Dill <marty@code-ninja.org>
2012-11-07 22:08:03 -0800

    Initial Commit

---
+++ ios-queryable/ios_queryable.h
@@ -0,0 +1,13 @@
+//
+//  ios_queryable.h
+//  ios-queryable
+//
+//  Created by Marty on 2012-11-07.
+//  Copyright (c) 2012 Marty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ios_queryable : NSObject
+
+@end
---
+++ ios-queryableTests/ios_queryableTests.h
@@ -0,0 +1,13 @@
+//
+//  ios_queryableTests.h
+//  ios-queryableTests
+//
+//  Created by Marty on 2012-11-07.
+//  Copyright (c) 2012 Marty. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ios_queryableTests : SenTestCase
+
+@end
